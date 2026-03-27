48c5734f2d578746f7b63db09d000a9183449e6d
saurabhuttam87 <uttamsaurabh495@gmail.com>
2026-03-27 10:32:13 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import {inngest, functions} from './inngest/index.js';
 
 const app = express();
 
@@ -10,9 +11,11 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=> res.send('Server is running'));
+app.get('/', (_, res)=> res.send('Server is running'));
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 });
