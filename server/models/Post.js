8489086122d47ffbaf455e7ef21b3d3bd6819e6e
saurabhuttam87 <uttamsaurabh495@gmail.com>
2026-03-29 12:33:06 +0530

    Commited

---
+++ server/models/Post.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    user: {type: String, ref: 'User', required: true},
+    content: {type: String},
+    image_urls: [{type: String}],
+    post_type: {type: String,enum: ['text', 'image','text_with_image'], required: true},
+})
